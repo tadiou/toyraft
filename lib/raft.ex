74a255f60a5e20827da3a708920846d6c99d823c
Dan Pozzie <dpozzie@gmail.com>
2022-06-10 21:46:54 -0400

    Init commit

---
+++ lib/raft.ex
@@ -0,0 +1,18 @@
+defmodule Raft do
+  @moduledoc """
+  Documentation for `Raft`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Raft.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
